8736a13e58f2b1ea033ce9a596a3a1a775c14222
Denys Konovalov <augmentido@gmail.com>
2023-09-20 18:17:35 +0200

    Controller is ready to use

--- Controller/Tasks.php
+++ Controller/Tasks.php
@@ -1,34 +1,129 @@
 <?php
 namespace Controller\Tasks;
 
+require_once ROOT . '/Model/Task.php';
+use \Model\Task as Task;
+
 class Tasks{
-    public function list($params){
-        echo 'Hi! This is list method';
-        print_r($params);
-
+    /**
+     * Method for getting a list of taks
+     * @return array of taskNodes
+     */
+    public function list(){
+        $t = new Task();
+        $tasks = $t->getList();
+        return Responder::respond('list', $tasks);
     }
 
-    public function add($params){
-        echo 'Hi! This is add method';
-        print_r($params);
+    /**
+     * Add a new task
+     * @return id
+     */
+    public function add(){
+        $input = json_decode(file_get_contents('php://input'),1);
+        if(is_null($input)){
+            return Responder::e400('Input data does not received or have invalid format.');
+        }
 
+        if(!isset($input['content']) OR !isset($input['done'])){
+            return Responder::e400('Input data does not contain required fields.');
+        }
+
+        $t = new Task();
+        $id = $t->add([
+            'done' => $input['done'] ? true : false,
+            'content' => $input['content'],
+        ]);
+        if($id === false){
+            return Responder::e500('Can`t add your task! Try again later.');
+        }
+        return Responder::respond('operation_result', [
+            'operation_type' => 'add',
+            'id' => $id,
+        ]);
     }
 
+    /**
+     * Get a single task by ID
+     * @param array $params = [id:int]
+     * 
+     * @return taskNode
+     */
     public function get($params){
-        echo 'Hi! This is get method';
-        print_r($params);
-
+        if(!isset($params['id'])){
+            return Responder::e400('Your request does not contain an ID.');
+        }
+        $params['id'] = intval($params['id']);
+        $t = new Task();
+        $taskSign = $t->get($params['id']);
+        if(is_null($taskSign)){
+            return Responder::e400('Item with your ID does not exist!');
+        }
+        return Responder::respond('task', [
+            'data' => $taskSign,
+        ]);
     }
 
+    /**
+     * Update a task by ID
+     * @param array $params = [id:int]
+     * 
+     * @return id:int
+     */
     public function update($params){
-        echo 'Hi! This is update method';
-        print_r($params);
+        $input = json_decode(file_get_contents('php://input'),1);
+        if(is_null($input)){
+            return Responder::e400('Input data does not received or have invalid format.');
+        }
 
+        if(!isset($input['content']) OR !isset($input['done']) OR !isset($params['id'])){
+            return Responder::e400('Input data does not contain required fields.');
+        }
+        $params['id'] = intval($params['id']);
+        $t = new Task();
+        $oldTaskSign = $t->get($params['id']);
+        if(is_null($oldTaskSign)){
+            return Responder::e400('Item with your ID does not exist!');
+        }
+
+        $res = $t->update([
+            'id' => $params['id'],
+            'done' => $input['done'] ? true : false,
+            'content' => $input['content'],
+        ]);
+        if(!$res){
+            return Responder::e500('Can`t update your task! Try again later.');
+        }
+        return Responder::respond('operation_result', [
+            'operation_type' => 'update',
+            'id' => $params['id'],
+        ]);
     }
 
+    /**
+     * Delete task by ID
+     * @param array $params = [id:int]
+     * 
+     * @return [status:string, id:int]
+     */
     public function delete($params){
-        echo 'Hi! This is delete method';
-        print_r($params);
-
+        if(!isset($params['id'])){
+            return Responder::e400('Your request does not contain an ID.');
+        }
+        $params['id'] = intval($params['id']);
+        $t = new Task();
+        $taskSign = $t->get($params['id']);
+        if(is_null($taskSign)){
+            return Responder::e400('Item with your ID does not exist!');
+        }
+        $res = $t->remove($params['id']);
+        if(!$res){
+            return Responder::e500('Can`t remove your task! Try again later.');
+        }
+        return Responder::respond('operation_result', [
+            'operation_type' => 'delete',
+            'status' => 'success',
+            'id' => $params['id']
+        ]);
     }
 }
