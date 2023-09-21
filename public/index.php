c0cabe7f0a54474c7bd4c7c85330fee0ad8b6e9b
Denys Konovalov <augmentido@gmail.com>
2023-09-21 11:25:52 +0200

    api (backend) code moved to /api folder to make a room for frontend.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
 
 define('PUBLIC_ROOT', __DIR__);
-include '../app.php';
+
+if(str_starts_with($_SERVER['PATH_INFO'], '/tasks')){
+    // backend api goes here
+    include '../api/app.php';
+}else{
+    // TODO: frontend will be here
+    echo 'Frontend';
+}
